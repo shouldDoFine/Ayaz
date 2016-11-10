88c20e42d2407bde49a6b489ae033c764d99ed1e
ayaz <badrutdinov.ayaz@yadnex.ru>
2016-11-10 22:42:38 +0300

    commented exec-maven-plugin

--- src/main/java/ru/ayaz/MessageProcessor.java
+++ src/main/java/ru/ayaz/MessageProcessor.java
@@ -1,27 +1,25 @@
 package ru.ayaz;
 
-import java.util.ArrayList;
-
 public class MessageProcessor {
 
-    public boolean isCommand(String message){
+    public boolean isCommand(String message) {
         message = message.trim();
         return message.startsWith("#");
     }
 
-    public String getCommand(String message){
+    public String getCommand(String message) {
         message = message.trim();
-        if(message.contains(" ")){
+        if (message.contains(" ")) {
             return message.substring(0, message.indexOf(" "));
-        }else{
+        } else {
             return message;
         }
     }
 
-    public String getFirstArgument(String message){
+    public String getFirstArgument(String message) {
         message = message.trim();
         message = message.substring(message.indexOf(" ") + 1);
-        if(message.contains(" ")){
+        if (message.contains(" ")) {
             message = message.substring(0, message.indexOf(" "));
         }
         return message;
--- src/test/java/ru/ayaz/MessageProcessorTest.java
+++ src/test/java/ru/ayaz/MessageProcessorTest.java
@@ -1,10 +1,7 @@
 package ru.ayaz;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-
-import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
@@ -17,38 +14,37 @@ public class MessageProcessorTest {
     }
 
     @Test
-    public void falseWhenSharpNotFirst(){
+    public void falseWhenSharpNotFirst() {
         assertEquals(false, processor.isCommand("d#quit"));
     }
 
     @Test
-    public void trueWhenSharpFirstAfterSpaces(){
+    public void trueWhenSharpFirstAfterSpaces() {
         assertEquals(true, processor.isCommand("    #quit"));
     }
 
     @Test
-    public void shouldGetCommandWhenNormalStartSpaces(){
+    public void shouldGetCommandWhenNormalStartSpaces() {
         assertEquals("#quit", processor.getCommand("    #quit"));
     }
 
     @Test
-    public void shouldGetCommandWhenNormalTwoWords(){
+    public void shouldGetCommandWhenNormalTwoWords() {
         assertEquals("#ignore", processor.getCommand("#ignore spammer"));
     }
 
 
     @Test
-    public void shouldGetArgumentWhenOneArgument(){
+    public void shouldGetArgumentWhenOneArgument() {
         String result = processor.getFirstArgument("  #ignore spammer");
         assertEquals("spammer", result);
     }
 
     @Test
-    public void shouldGetFirstArgumentWhenTwoArguments(){
+    public void shouldGetFirstArgumentWhenTwoArguments() {
         String result = processor.getFirstArgument("  #ignore spammer badGuy123");
         assertEquals("spammer", result);
     }
-
 
 
 }
