3bdb4b5dfe39e2e16f1a7f218656d7be332a6f81
ayaz <badrutdinov.ayaz@yadnex.ru>
2016-11-20 16:41:22 +0300

    Changing architecture: Moved registration logic to ChatServer class. UserMessageHandler will implement queueEvent pattern.

--- src/main/java/ru/ayaz/UserHandler.java
+++ src/main/java/ru/ayaz/UserHandler.java
@@ -2,78 +2,32 @@ package ru.ayaz;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
 
 
 public class UserHandler implements Runnable {
 
     private User user;
-    private ChatServer chat;
-    private MessageProcessor processor;
-    private PrintWriter writer;
     private BufferedReader reader;
 
 
-    public UserHandler(Socket socket) {
-        try {
-            this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            this.writer = new PrintWriter(socket.getOutputStream());
-            this.user = new User();
-            this.processor = new MessageProcessor();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public UserHandler(User user) {
+            this.user = user;
     }
 
-
-    public void setChatReference(ChatServer chat) {
-        this.chat = chat;
+    public void setReader(BufferedReader reader) {
+        this.reader = reader;
     }
-
 
     public void run() {
-
-        welcomeUser();
-
-        try {
-
-            user.setNickname(listenForMessage());
-            chat.registerUser(user, writer);
-            startListeningToUser();
-
-        } catch (InvalidNicknameException e) {
-            writeSystemMessageToThisUser(e.toString());
-            try {
-                closeWriterAndReader();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
+        //startListeningToUser();
     }
-
-
-    private void welcomeUser() {
-        writeSystemMessageToThisUser("Welcome to Chat" + "Please enter your name:\n");
-    }
-
-
-    private void writeSystemMessageToThisUser(String message) {
-        writer.println(message);
-        writer.flush();
-    }
-
 
     private String listenForMessage() throws IOException {
         return reader.readLine();
     }
 
 
+    /*
     private void sayEveryoneThatUserHasConnected(String connectedUser) {
         chat.sendMessageToEveryone(connectedUser + " connected to chat", connectedUser);
     }
@@ -87,7 +41,7 @@ public class UserHandler implements Runnable {
     private void executeCommand(String commandMessage) throws IOException {
         switch (processor.getCommand(commandMessage)) {
             case "#quit":
-                closeWriterAndReader();
+                closeReader();
                 break;
             case "#ignore":
                 try {
@@ -121,10 +75,10 @@ public class UserHandler implements Runnable {
             sayEveryoneThatUserHasLeft(sender);
         }
     }
+    */
 
 
-    private void closeWriterAndReader() throws IOException {
-        writer.close();
+    private void closeReader() throws IOException {
         reader.close();
     }
 
